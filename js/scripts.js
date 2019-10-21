92c20c6a0167970cfb9233fab3ac6a081187a613
Lucas <lvalandrorocha@hotmail.com>
2019-10-20 21:36:07 -0300

    Add courses and specializations section

--- js/scripts.js
+++ js/scripts.js
@@ -3,40 +3,44 @@ const EN = 0;
 const PT = 1;
 const menu = [
     {
-        "about": "ABOUT"
+        "about": "ABOUT",
+        "courses": "COURSES",
+        "courses-title": "COURSES AND SPECIALIZATIONS"
     },
     {
-        "about": "SOBRE"
+        "about": "SOBRE",
+        "courses": "CURSOS",
+        "courses-title": "CURSOS E ESPECIALIZAÇÕES"
     }
 ];
 const text = [
     {
-        "age": "I'm 22 years old.",
+        "age": "I'm 23 years old.",
         "student": "Student of ",
         "course": "Computer Engineering at UFRGS",
         "working": "Currently working as a ",
-        "job": "Back-End developer",
+        "job": "Software Engineer",
         "using": " using ",
-        "tech": "Java 10",
+        "tech": "Java",
         "activity": " to build microservices",
         "ide": "Co-founder of Empresa Júnior IDE.",
         "info": "For more information:" 
     },
     {
-        "age": "Eu tenho 22 anos de idade.",
+        "age": "Eu tenho 23 anos de idade.",
         "student": "Estudante de ",
         "course": "Engenharia da Computação na UFRGS",
         "working": "Atualmente trabalhando como ",
-        "job": "desenvolverdor Back-End",
+        "job": "engenheiro de Software",
         "using": " utilizando ",
-        "tech": "Java 10",
+        "tech": "Java",
         "activity": " para construção de microsserviços",
         "ide": "Co-fundador da Empresa Júnior IDE.",
         "info": "Para mais informações:" 
     }
 ];
 // Functions
-function showMenu(){
+function showMenu() {
     const button = document.getElementById('dropdown-menu');
     const links  = document.getElementById('links');
 
@@ -46,9 +50,10 @@ function showMenu(){
         links.classList.add('hidden');
     }
 }
+
 document.getElementById('dropdown-menu').addEventListener('click',showMenu);
 
-function changeLanguage(){
+function changeLanguage() {
     const en = document.getElementById('en');
     const pt = document.getElementById('pt');
     const en_m = document.getElementById('en-m');
@@ -68,20 +73,60 @@ function changeLanguage(){
         en_m.classList.add('language-selected');
     }
 }
-document.getElementById('en').addEventListener('click',changeLanguage);
-document.getElementById('en-m').addEventListener('click',changeLanguage);
-document.getElementById('pt').addEventListener('click',changeLanguage);
-document.getElementById('pt-m').addEventListener('click',changeLanguage);
 
 // Function for change HTML tags content
-function changeTexts(menu,texts){
-    for(var i in menu){
+function changeTexts(menu, texts) {
+    for(var i in menu) {
         var elements = Array.from(document.getElementsByClassName(i));
         elements.forEach((el) => {
             el.innerHTML = menu[i];
         });
     }
-    for(var i in texts){
+    for(var i in texts) {
         document.getElementById(i).innerHTML = texts[i];
     }
 }
+
+document.getElementById('en').addEventListener('click',changeLanguage);
+document.getElementById('en-m').addEventListener('click',changeLanguage);
+document.getElementById('pt').addEventListener('click',changeLanguage);
+document.getElementById('pt-m').addEventListener('click',changeLanguage);
+
+function changeMenuAbout() {
+    const aboutSection = document.getElementById('about');
+    const coursesSection = document.getElementById('courses');
+    const aboutButton = document.getElementById('about-button');
+    const aboutButtonM = document.getElementById('about-button-m');
+    const courseButton = document.getElementById('courses-button');
+    const courseButtonM = document.getElementById('courses-button-m');
+    if (aboutSection.classList.contains('hidden')) {
+        coursesSection.classList.add('hidden');
+        aboutSection.classList.remove('hidden');
+        courseButton.classList.remove('menu-selected');
+        courseButtonM.classList.remove('menu-selected');
+        aboutButton.classList.add('menu-selected');
+        aboutButtonM.classList.add('menu-selected');        
+    }
+}
+
+function changeMenuCourse() {
+    const aboutSection = document.getElementById('about');
+    const coursesSection = document.getElementById('courses');
+    const aboutButton = document.getElementById('about-button');
+    const aboutButtonM = document.getElementById('about-button-m');
+    const courseButton = document.getElementById('courses-button');
+    const courseButtonM = document.getElementById('courses-button-m');
+    if (coursesSection.classList.contains('hidden')) {
+        aboutSection.classList.add('hidden');
+        coursesSection.classList.remove('hidden');
+        courseButton.classList.add('menu-selected');
+        courseButtonM.classList.add('menu-selected');
+        aboutButton.classList.remove('menu-selected');
+        aboutButtonM.classList.remove('menu-selected');        
+    }
+}
+
+document.getElementById('about-button').addEventListener('click', changeMenuAbout);
+document.getElementById('about-button-m').addEventListener('click', changeMenuAbout);
+document.getElementById('courses-button').addEventListener('click', changeMenuCourse);
+document.getElementById('courses-button-m').addEventListener('click', changeMenuCourse);
